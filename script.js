83ebc09dcc217db4d01eef574edeb523ee1486ab
SerafimPoch <serpochapinsky@gmail.com>
2018-02-06 17:38:34 +0200

    fix twitter button bug

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
-const quote     = document.querySelector('.quote')
-const author    = document.querySelector('.author')
-const new_quote = document.querySelector('.new_quote')
-const twit      = document.querySelector('.twit')
-const random    = 'https://random-quote-generator.herokuapp.com/api/quotes/random'
-const twitter   = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
+const quote     = document.querySelector(".quote");
+const author    = document.querySelector(".author");
+const new_quote = document.querySelector(".new_quote");
+const twit      = document.querySelector(".twit");
+const random    = "https://random-quote-generator.herokuapp.com/api/quotes/random";
+const twitter   = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=";
 
-
-const randomQuote = async x => {
-    let response = await fetch(x)
-    let data = await response.json()
-        buttonClick(data)
-
-        console.log(data)                 
+const getRandomQuote = () => {
+  return fetch(random)
+    .then(x => x.json()) 
 }
 
-
- 
-const buttonClick = x => {
-    let dark =  () => {
-        quote.innerHTML  = x.quote
-        author.innerHTML = x.author
-    new_quote.removeEventListener('click',dark) 
-     randomQuote(random)
-    }
-    new_quote.addEventListener('click',dark)
-   twitButton(quote.innerHTML)
-    
+const renderQuote = x => {
+  quote.innerText  = x.quote
+  author.innerText = x.author
 }
 
-const twitButton = x => {
-    twit.addEventListener('click', () => {
-        window.open(twitter+x)
-    })
-}
+new_quote.addEventListener('click', async el => {
+  const nextQuote = await getRandomQuote()
+  renderQuote(nextQuote)
+})
 
-
-
-
-
-
-randomQuote(random)
+twit.addEventListener('click', () => {
+   window.open(twitter + quote.innerText)
+})
